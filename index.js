0a5adfb5b25f91dd9155e80634c5a4540fbffc85
Hushibul <nayeemninz786@gmail.com>
2023-11-05 14:54:02 +0600

    env file removed

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const dotenv = require('dotenv');
 const express = require('express');
 
 const errorHandler = require('./middlewares/errorHandler');
 const Routes = require('./routes/router');
 
-dotenv.config();
+const PORT = 5000;
 
 const app = express();
 app.use(express.json());
@@ -17,9 +16,7 @@ app.get('/api', (req, res) => {
   res.render('index');
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server is running at ${process.env.PORT}`)
-);
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
 
 //Error Middleware
 app.use(errorHandler);
