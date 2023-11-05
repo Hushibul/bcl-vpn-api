a3fe8c77be28f07c908431cf6114f8a1bda34240
azadbcl <146822160+azadbcl@users.noreply.github.com>
2023-11-05 15:56:39 +0600

    apply script for file deletion

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,6 @@
 // const { readAllFiles } = require('../utils/utils');
 const { exec } = require('child_process');
-const command = 'find ../uploads -type f -mmin +5 -delete';
+// const command = 'find ../uploads -type f -mmin +5 -delete';
 
 const uploadFile = async (req, res, next) => {
   res.render('index', { message: 'File Uploaded Successfully' });
@@ -13,23 +13,44 @@ const uploadFile = async (req, res, next) => {
 
 const deleteFile = async (req, res, next) => {
   try {
+    const command = `
+        parentDirectory=$(dirname "$(pwd)")
+        absolutePath="$parentDirectory/uploads"
+        numberOfDeletedFile=0
+        
+        for file in "$absolutePath"/*; do
+          if [ -f "$file" ]; then
+            fileCreateTime=$(stat -f "%B" "$file")
+            curTime=$(date +%s)
+            timeDiffInMinutes=$(( (curTime - fileCreateTime) / 60 ))
+            
+            if [ $timeDiffInMinutes -le 30 ]; then
+             if rm "$file"; then
+             ((numberOfDeletedFile++))
+              fi
+            fi
+          fi
+        done
+        
+        echo "Deleted $numberOfDeletedFile files."
+        `;
     exec(command, (error, stdout, stderr) => {
       if (error) {
-        console.error(`Error: ${error.message}`);
-        return;
+        console.log(`Error executing the command: ${error.message}`);
+        return res.status(400).json({ message: "Error executing the command" });
       }
       if (stderr) {
-        console.error(`Error: ${stderr}`);
-        return;
+        console.error(`Command execution resulted in an error: ${stderr}`);
+        return res.status(500).send("Command execution error");
       }
       console.log(`Command output: ${stdout}`);
-      // res.status(200).json({ success: true, output: stdout });
+      res
+        .status(200)
+        .json({ count: stdout, message: "Delete Operation Successfully Run" });
     });
-
-    // readAllFiles();
-  } catch (err) {
-    next(err);
+  } catch (error) {
+    res.status(400).json({ message: error });
   }
 };
 
-module.exports = { uploadFile, deleteFile };
+module.exports = { uploadFile, deleteFile};
